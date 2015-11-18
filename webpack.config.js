f176fa7fd332f30f97d5e7c997c8926ab45018b7
geowarin <geowarin@gmail.com>
2015-11-18 22:16:44 +0100

    Extract css to a separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var jeet = require('jeet');
 var nib = require('nib');
 
@@ -27,20 +28,27 @@ module.exports = {
             compressor: {
                 warnings: false
             }
-        })
+        }),
+        new ExtractTextPlugin("styles.css")
     ],
     resolve: {
         extensions: ['', '.js'],
         root: path.join(__dirname, 'src')
     },
     module: {
+        preLoaders: [
+            {
+                test: /\.(js|css)$/,
+                loader: 'stripcomment'
+            }
+        ],
         loaders: [{
             test: /\.js$/,
             loaders: ['babel'],
             include: path.join(__dirname, 'src')
         }, {
             test: /\.styl$/,
-            loaders: ['style-loader', 'css-loader', 'stylus-loader']
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader', 'stylus-loader')
         }]
     },
     stylus: {
